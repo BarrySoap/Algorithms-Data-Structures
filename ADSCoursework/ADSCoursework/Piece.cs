df9a457672e9d00967f721b6f41c366da1590573
LAPTOP-ATE4ASPG\glenn <barrysoap1@gmail.com>
2017-10-09 12:25:16 +0100

    COURSEWORK: Initial Commit - Frame with buttons, but no events or backend.

---
+++ ADSCoursework/ADSCoursework/Piece.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADSCoursework
+{
+    class Piece
+    {
+        private float position;
+        private float newPosition;
+        private string colour;
+
+    }
+}
