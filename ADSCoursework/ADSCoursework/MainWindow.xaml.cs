8b1e55036c7f9ef13a48830201a8cd81ea636a56
LAPTOP-ATE4ASPG\glenn <barrysoap1@gmail.com>
2017-10-09 12:31:27 +0100

    GUI Class now has every button event. Messy, but necessary.

--- ADSCoursework/ADSCoursework/MainWindow.xaml.cs
+++ ADSCoursework/ADSCoursework/MainWindow.xaml.cs
@@ -29,5 +29,320 @@ namespace ADSCoursework
         {
             MessageBox.Show("Test Button");
         }
+
+        private void btnCell2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell10_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell11_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell12_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell13_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell14_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell15_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell16_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell17_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell18_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell19_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell20_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell21_Click(object sender, RoutedEventArgs e)
+        {
+                
+        }
+
+        private void btnCell22_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell23_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell24_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell25_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell26_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell27_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell28_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell29_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell30_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell31_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell32_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell33_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell34_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell35_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell36_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell37_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell38_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell39_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell40_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell41_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell42_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell43_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell44_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell45_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell46_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell47_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell48_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell49_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell50_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell51_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell52_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell53_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell54_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell55_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell56_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell57_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell58_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell59_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell60_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell61_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell62_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell63_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCell64_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
