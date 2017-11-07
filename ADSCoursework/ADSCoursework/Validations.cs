e982f803f61950031e8af3028fecbdf7e58dd0d2
Glenn Wilkie-Sullivan <glennpws@hotmail.co.uk>
2017-11-07 12:24:15 +0000

    Fixed an issue with the "Can a piece be taken" method going out of bounds.

--- ADSCoursework/ADSCoursework/Validations.cs
+++ ADSCoursework/ADSCoursework/Validations.cs
@@ -113,39 +113,127 @@ namespace ADSCoursework
             }
         }
 
+        public static bool IsKingMoveValid(Button cell, Piece currentPiece, Button[] buttonList, int oldPosition, int newPosition, ref bool pieceTaken,
+            List<Piece> whitePieces, List<Piece> blackPieces)
+        {
+            int temp = 0;
+            int temp2 = 0;
+
+            if (currentPiece.IsPieceKing() == true)
+            {
+                if (currentPiece.GetColour() == "White")
+                {
+                    if (oldPosition - newPosition == 7 || oldPosition - newPosition == 9 ||
+                        oldPosition - newPosition == -7 || oldPosition - newPosition == -9 && buttonList[newPosition].Background == Brushes.Gray)
+                    {
+                        return true;
+                    }
+                    else if (oldPosition - newPosition == 14 || oldPosition - newPosition == 18 ||
+                             oldPosition - newPosition == -14 || oldPosition - newPosition == -18)
+                    {
+                        if (buttonList[newPosition + 9].Background == Brushes.Black || buttonList[newPosition + 7].Background == Brushes.Black ||
+                            buttonList[newPosition - 9].Background == Brushes.Black || buttonList[newPosition - 7].Background == Brushes.Black)
+                        {
+                            pieceTaken = true;
+                            temp = Convert.ToInt32(currentPiece.GetPosition());
+                            temp2 = temp + Convert.ToInt32(currentPiece.GetNewPosition());
+                            temp2 /= 2;
+
+                            for (int i = 0; i < blackPieces.Count; i++)
+                            {
+                                if (Convert.ToInt32(blackPieces[i].GetNewPosition()) == temp2 ||
+                                    Convert.ToInt32(blackPieces[i].GetPosition()) == temp2)
+                                {
+                                    blackPieces[i].SetTaken(true);
+                                }
+                            }
+                            return true;
+                        }
+                        return false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                } else if (currentPiece.GetColour() == "Black")
+                {
+                    if (oldPosition - newPosition == 7 || oldPosition - newPosition == 9 ||
+                        oldPosition - newPosition == -7 || oldPosition - newPosition == -9 && buttonList[newPosition].Background == Brushes.Gray)
+                    {
+                        return true;
+                    }
+                    else if (oldPosition - newPosition == 14 || oldPosition - newPosition == 18 ||
+                             oldPosition - newPosition == -14 || oldPosition - newPosition == -18)
+                    {
+                        if (buttonList[newPosition + 9].Background == Brushes.White || buttonList[newPosition + 7].Background == Brushes.White ||
+                            buttonList[newPosition - 9].Background == Brushes.White || buttonList[newPosition - 7].Background == Brushes.White)
+                        {
+                            pieceTaken = true;
+                            temp = Convert.ToInt32(currentPiece.GetPosition());
+                            temp2 = temp + Convert.ToInt32(currentPiece.GetNewPosition());
+                            temp2 /= 2;
+
+                            for (int i = 0; i < whitePieces.Count; i++)
+                            {
+                                if (Convert.ToInt32(whitePieces[i].GetNewPosition()) == temp2 ||
+                                    Convert.ToInt32(whitePieces[i].GetPosition()) == temp2)
+                                {
+                                    whitePieces[i].SetTaken(true);
+                                }
+                            }
+                            return true;
+                        }
+                        return false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                } else
+                {
+                    return false;
+                }
+            } else
+            {
+                return false;
+            }
+        }
+
         public static void CanPieceBeTaken(Button cell, ref Piece currentPiece, Button[] buttonList)
         {
-            if (currentPiece.GetColour() == "White" && buttonList[Convert.ToInt32(currentPiece.GetPosition()) - 7].Background == Brushes.Black 
-                && Convert.ToInt32(currentPiece.GetPosition()) - 14 > 0)
+            if (currentPiece.GetColour() == "White")
             {
-                if (buttonList[Convert.ToInt32(currentPiece.GetPosition()) - 14].Background == Brushes.Gray)
+                if (Convert.ToInt32(currentPiece.GetPosition()) - 7 > 0 && buttonList[Convert.ToInt32(currentPiece.GetPosition()) - 7].Background == Brushes.Black)
                 {
-                    buttonList[Convert.ToInt32(currentPiece.GetPosition()) - 7].Background = Brushes.Cyan;
+                    if (Convert.ToInt32(currentPiece.GetPosition()) - 14 > 0 && buttonList[Convert.ToInt32(currentPiece.GetPosition()) - 14].Background == Brushes.Gray)
+                    {
+                        buttonList[Convert.ToInt32(currentPiece.GetPosition()) - 7].Background = Brushes.Cyan;
+                    }
                 }
-            }
-            if (currentPiece.GetColour() == "White" && buttonList[Convert.ToInt32(currentPiece.GetPosition()) - 9].Background == Brushes.Black
-                 && Convert.ToInt32(currentPiece.GetPosition()) - 18 > 0)
-            {
-                if (buttonList[Convert.ToInt32(currentPiece.GetPosition()) - 18].Background == Brushes.Gray)
+                if (Convert.ToInt32(currentPiece.GetPosition()) - 9 > 0 && buttonList[Convert.ToInt32(currentPiece.GetPosition()) - 9].Background == Brushes.Black)
                 {
-                    buttonList[Convert.ToInt32(currentPiece.GetPosition()) - 9].Background = Brushes.Cyan;
+                    if (Convert.ToInt32(currentPiece.GetPosition()) - 18 > 0 && buttonList[Convert.ToInt32(currentPiece.GetPosition()) - 18].Background == Brushes.Gray)
+                    {
+                        buttonList[Convert.ToInt32(currentPiece.GetPosition()) - 9].Background = Brushes.Cyan;
+                    }
                 }
             }
 
-            if (currentPiece.GetColour() == "Black" && buttonList[Convert.ToInt32(currentPiece.GetPosition()) + 7].Background == Brushes.White
-                 && Convert.ToInt32(currentPiece.GetPosition()) + 14 > 63)
+            if (currentPiece.GetColour() == "Black")
             {
-                if (buttonList[Convert.ToInt32(currentPiece.GetPosition()) + 14].Background == Brushes.Gray)
+                if (Convert.ToInt32(currentPiece.GetPosition()) + 7 < 63 && buttonList[Convert.ToInt32(currentPiece.GetPosition()) + 7].Background == Brushes.White)
                 {
-                    buttonList[Convert.ToInt32(currentPiece.GetPosition()) + 7].Background = Brushes.Cyan;
+                    if (Convert.ToInt32(currentPiece.GetPosition()) + 14 < 63 && buttonList[Convert.ToInt32(currentPiece.GetPosition()) + 14].Background == Brushes.Gray)
+                    {
+                        buttonList[Convert.ToInt32(currentPiece.GetPosition()) + 7].Background = Brushes.Cyan;
+                    }
                 }
-            }
-            if (currentPiece.GetColour() == "Black" && buttonList[Convert.ToInt32(currentPiece.GetPosition()) + 9].Background == Brushes.White
-                 && Convert.ToInt32(currentPiece.GetPosition()) + 18 > 63)
-            {
-                if (buttonList[Convert.ToInt32(currentPiece.GetPosition()) + 18].Background == Brushes.Gray)
+                if (Convert.ToInt32(currentPiece.GetPosition()) + 9 < 63 && buttonList[Convert.ToInt32(currentPiece.GetPosition()) + 9].Background == Brushes.White)
                 {
-                    buttonList[Convert.ToInt32(currentPiece.GetPosition()) + 9].Background = Brushes.Cyan;
+                    if (Convert.ToInt32(currentPiece.GetPosition()) + 18 < 63 && buttonList[Convert.ToInt32(currentPiece.GetPosition()) + 18].Background == Brushes.Gray)
+                    {
+                        buttonList[Convert.ToInt32(currentPiece.GetPosition()) + 9].Background = Brushes.Cyan;
+                    }
                 }
             }
         }
